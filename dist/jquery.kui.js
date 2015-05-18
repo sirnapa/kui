110b8b2e53993d46543bb06621199e67141a07ed
Nelson Páez <nelpa90@gmail.com>
2015-05-18 15:11:58 -0400

    Ejemplos para v0.1.2

--- dist/jquery.kui.js
+++ dist/jquery.kui.js
@@ -1,4 +1,4 @@
-/*! kui - v0.1.1 - 2015-05-18
+/*! kui - v0.1.2 - 2015-05-18
 * https://github.com/konecta/kui
 * Copyright (c) 2015 Nelson Paez; Licensed MIT */
 (function ($) {
@@ -592,7 +592,7 @@
             if(o.params.seleccionable){
                 // Agregar campo de selección al principio;
                 var campo_seleccion = {
-                    nombre: 'kGrid_seleccionado',
+                    nombre: 'kui_seleccionado',
                     titulo: '',
                     tipo: 'booleano',
                     ancho: 1,
@@ -633,7 +633,7 @@
             o.list.enlace_dummy = 'javascript'+':'.toLowerCase()+'void(0)';
 
             $.kui.list.cargar_estilos();
-            o.list.cargar_paginador();
+            $.kui.list.cargar_paginador(o.list);
             o.list.titulos();
             o.list.cargar();
             
@@ -641,6 +641,105 @@
                 $.kui.instances.kgrid[o.list.id].cargar();
             });
             
+        },
+
+        cargar_paginador : function(list){
+            if(!$(list.paginador).length){
+                return;
+            }
+            var pk = 'kui_' + list.div.id + '_';
+            var contenedor = $('<div>').attr('id',pk+'paginador')
+                .addClass('kui-paginador btn-group')
+                .appendTo(list.paginador);
+            
+            $('<button>').attr('id',pk+'primera_pagina')
+                .attr('type','button')
+                .addClass('btn btn-default')
+                .html($('<i>').addClass('fa fa-step-backward'))
+                .appendTo(contenedor)
+                .click(function(){
+                    $('#'+list.div.id).kui(list.name,'pagina','primera');
+                });
+                
+           $('<button>').attr('id',pk+'pagina_anterior')
+                .attr('type','button')
+                .addClass('btn btn-default')
+                .html($('<i>').addClass('fa fa-backward'))
+                .appendTo(contenedor)
+                .click(function(){
+                    $('#'+list.div.id).kui(list.name,'pagina','anterior');
+                });
+        
+          var alto = $('#'+pk+'pagina_anterior').outerHeight();
+          var ancho = $('#'+pk+'pagina_anterior').outerWidth();
+          
+          var centro = $('<div>').addClass('btn btn-default kpagina')
+                .css('height',alto>0? alto : 34)
+                .appendTo(contenedor);
+
+          $('<label>').html('Página ').appendTo(centro);
+          $('<input>').attr('id',pk+'pagina')
+            .attr('type','text')
+            .addClass('pagina')
+            .css('width',ancho>0? ancho : 39)
+            .appendTo(centro)
+            .keyup(function(e){
+                if(e.keyCode === 13){
+                    e.preventDefault();
+                    var pagina = parseInt($('#'+pk+'pagina').val());
+                    if(isNaN(pagina) || pagina<0 || pagina > list.totalPaginas){
+                        $('#'+pk+'pagina').val($('#'+pk+'pagina').data('pagina'));
+                        return;
+                    }
+                    if(pagina!==$('#'+pk+'pagina').data('pagina')){
+                        $('#'+list.div.id).kui(list.name,'pagina',pagina);
+                    }
+                }
+            });
+          var totalPaginas = $('<label>').html(' de ').appendTo(centro);
+          $('<span>').attr('id',pk+'totalPaginas')
+            .appendTo(totalPaginas);
+                
+          $('<button>').attr('id',pk+'siguiente_pagina')
+                .attr('type','button')
+                .addClass('btn btn-default')
+                .html( $('<i>').addClass('fa fa-forward'))
+                .appendTo(contenedor)
+                .click(function(){
+                    $('#'+list.div.id).kui(list.name,'pagina','siguiente');
+                });
+                
+           $('<button>').attr('id',pk+'ultima_pagina')
+                .attr('type','button')
+                .addClass('btn btn-default')
+                .html($('<i>').addClass('fa fa-step-forward'))
+                .appendTo(contenedor)
+                .click(function(){
+                    $('#'+list.div.id).kui(list.name,'pagina','ultima');
+                });
+        },
+
+        refrescar_paginador: function(list){
+            window.console.log('Actual',list.pagina);
+            window.console.log('Primera',list.pagina===1);
+            window.console.log('Ultima',list.pagina===list.totalPaginas);
+            window.console.log('Total de paginas',list.totalPaginas);
+            window.console.log('Total de datos',list.totalDatos);
+            window.console.log('================================');
+
+            $('#kui_' + list.div.id + '_pagina')
+                .val(list.pagina)
+                .data('pagina',list.pagina);
+            $('#kui_' + list.div.id + '_totalPaginas')
+                .html(list.totalPaginas);
+            $('#kui_' + list.div.id + '_primera_pagina')
+                .prop('disabled',list.pagina===1);
+            $('#kui_' + list.div.id + '_pagina_anterior')
+                .prop('disabled',list.pagina===1);
+            $('#kui_' + list.div.id + '_ultima_pagina')
+                .prop('disabled',list.pagina===list.totalPaginas);
+            $('#kui_' + list.div.id + '_siguiente_pagina')
+                .prop('disabled',list.pagina===list.totalPaginas);
         },
 
         cargar_estilos: function(){
@@ -842,6 +941,8 @@
     };
     
     KCard.prototype = {
+
+        name: 'cards',
     		
         set_data: function(data){
             var kCard = this;
@@ -883,8 +984,8 @@
 
                         var lista = retorno.respuesta.datos;
                         var datos = {};
-                        kCard.totalDatos = retorno.respuesta.totalDatos;
-                        kCard.pagina = retorno.respuesta.pagina;
+                        kCard.totalDatos = parseInt(retorno.respuesta.totalDatos);
+                        kCard.pagina = parseInt(retorno.respuesta.pagina);
                         kCard.totalPaginas = Math.ceil(kCard.totalDatos/kCard.data.rows);
 
                         kCard.grilla = $('<div>').addClass('kCard').prependTo(kCard.contenido);                       
@@ -928,19 +1029,7 @@
                         $(kCard.div).data('pagina',kCard.pagina);
                         $(kCard.div).data('totalPaginas',kCard.totalPaginas);
                                                 
-                        $('#kCard_' + kCard.div.id + '_pagina')
-                            .val(kCard.pagina)
-                            .data('pagina',kCard.pagina);
-                        $('#kCard_' + kCard.div.id + '_totalPaginas')
-                            .html(kCard.totalPaginas);
-                        $('#kCard_' + kCard.div.id + '_primera_pagina')
-                            .attr('disabled',kCard.pagina===1);
-                        $('#kCard_' + kCard.div.id + '_pagina_anterior')
-                            .attr('disabled',kCard.pagina===1);
-                        $('#kCard_' + kCard.div.id + '_ultima_pagina')
-                            .attr('disabled',kCard.pagina===kCard.totalPaginas);
-                        $('#kCard_' + kCard.div.id + '_siguiente_pagina')
-                            .attr('disabled',kCard.pagina===kCard.totalPaginas);
+                        $.kui.list.refrescar_paginador(kCard);
                         
                     }else if(retorno.mensaje){
                         $.kui.messages(kCard.mensaje,kCard.contenido,retorno.tipoMensaje,retorno.mensaje);
@@ -1277,80 +1366,6 @@
             }
         },
         
-        cargar_paginador : function(){
-            var kCard = this;    
-            if(!$(kCard.paginador).length){
-                return;
-            }
-            var pk = 'kCard_' + kCard.div.id + '_';
-            var contenedor = $('<div>').attr('id',pk+'paginador')
-                .addClass('kCard-paginador btn-group')
-                .appendTo(kCard.paginador);
-            
-            $('<button>').attr('id',pk+'primera_pagina')
-                .attr('type','button')
-                .addClass('btn btn-default')
-                .html($('<i>').addClass('fa fa-step-backward'))
-                .appendTo(contenedor)
-                .click(function(){
-                    $('#'+kCard.div.id).kCard('pagina','primera');
-                });
-                
-           $('<button>').attr('id',pk+'pagina_anterior')
-                .attr('type','button')
-                .addClass('btn btn-default')
-                .html($('<i>').addClass('fa fa-backward'))
-                .appendTo(contenedor)
-                .click(function(){
-                    $('#'+kCard.div.id).kCard('pagina','anterior');
-                });
-                
-          var centro = $('<div>').addClass('btn btn-default kpagina')
-                .css('height',$('#'+pk+'pagina_anterior').outerHeight())
-                .appendTo(contenedor);
-                
-          $('<label>').html('Página ').appendTo(centro);
-          $('<input>').attr('id',pk+'pagina')
-            .attr('type','text')
-            .addClass('pagina')
-            .css('width',$('#'+pk+'pagina_anterior').outerWidth())
-            .appendTo(centro)
-            .keyup(function(e){
-                if(e.keyCode === 13){
-                    e.preventDefault();
-                    var pagina = parseInt($('#'+pk+'pagina').val());
-                    if(isNaN(pagina) || pagina<0 || pagina > kCard.totalPaginas){
-                        $('#'+pk+'pagina').val($('#'+pk+'pagina').data('pagina'));
-                        return;
-                    }
-                    if(pagina!==$('#'+pk+'pagina').data('pagina')){
-                        $('#'+kCard.div.id).kCard('pagina',pagina);
-                    }
-                }
-            });
-          var totalPaginas = $('<label>').html(' de ').appendTo(centro);
-          $('<span>').attr('id',pk+'totalPaginas')
-            .appendTo(totalPaginas);
-                
-          $('<button>').attr('id',pk+'siguiente_pagina')
-                .attr('type','button')
-                .addClass('btn btn-default')
-                .html( $('<i>').addClass('fa fa-forward'))
-                .appendTo(contenedor)
-                .click(function(){
-                    $('#'+kCard.div.id).kCard('pagina','siguiente');
-                });
-                
-           $('<button>').attr('id',pk+'ultima_pagina')
-                .attr('type','button')
-                .addClass('btn btn-default')
-                .html($('<i>').addClass('fa fa-step-forward'))
-                .appendTo(contenedor)
-                .click(function(){
-                    $('#'+kCard.div.id).kCard('pagina','ultima');
-                });
-        },
-
         cambiar_seleccion: function(codigo,estado){
             var kCard = this;
             kCard.seleccionados[codigo] = estado;
@@ -1648,6 +1663,8 @@
     };
     
     KGrid.prototype = {
+
+        name: 'grid',
     		
         set_data: function(data){
             var kGrid = this;
@@ -1747,8 +1764,8 @@
 
                         var lista = retorno.respuesta.datos;
                         var datos = {};
-                        kGrid.totalDatos = retorno.respuesta.totalDatos;
-                        kGrid.pagina = retorno.respuesta.pagina;
+                        kGrid.totalDatos = parseInt(retorno.respuesta.totalDatos);
+                        kGrid.pagina = parseInt(retorno.respuesta.pagina);
                         kGrid.totalPaginas = Math.ceil(kGrid.totalDatos/kGrid.data.rows);
                         
                         $.each(lista,function(i,item){
@@ -1771,19 +1788,7 @@
                         $(kGrid.div).data('pagina',kGrid.pagina);
                         $(kGrid.div).data('totalPaginas',kGrid.totalPaginas);
                                                 
-                        $('#kGrid_' + kGrid.div.id + '_pagina')
-                            .val(kGrid.pagina)
-                            .data('pagina',kGrid.pagina);
-                        $('#kGrid_' + kGrid.div.id + '_totalPaginas')
-                            .html(kGrid.totalPaginas);
-                        $('#kGrid_' + kGrid.div.id + '_primera_pagina')
-                            .attr('disabled',kGrid.pagina===1);
-                        $('#kGrid_' + kGrid.div.id + '_pagina_anterior')
-                            .attr('disabled',kGrid.pagina===1);
-                        $('#kGrid_' + kGrid.div.id + '_ultima_pagina')
-                            .attr('disabled',kGrid.pagina===kGrid.totalPaginas);
-                        $('#kGrid_' + kGrid.div.id + '_siguiente_pagina')
-                            .attr('disabled',kGrid.pagina===kGrid.totalPaginas);
+                        $.kui.list.refrescar_paginador(kGrid);
                         
                     }else if(retorno.mensaje){
                         $.kui.messages(kGrid.mensaje,kGrid.tbody,retorno.tipoMensaje,retorno.mensaje);
@@ -2229,80 +2234,6 @@
 
         },
         
-        cargar_paginador : function(){
-            var kGrid = this;    
-            if(!$(kGrid.paginador).length){
-                return;
-            }
-            var pk = 'kGrid_' + kGrid.div.id + '_';
-            var contenedor = $('<div>').attr('id',pk+'paginador')
-                .addClass('kGrid-paginador btn-group')
-                .appendTo(kGrid.paginador);
-            
-            $('<button>').attr('id',pk+'primera_pagina')
-                .attr('type','button')
-                .addClass('btn btn-default')
-                .html($('<i>').addClass('fa fa-step-backward'))
-                .appendTo(contenedor)
-                .click(function(){
-                    $('#'+kGrid.div.id).kGrid('pagina','primera');
-                });
-                
-           $('<button>').attr('id',pk+'pagina_anterior')
-                .attr('type','button')
-                .addClass('btn btn-default')
-                .html($('<i>').addClass('fa fa-backward'))
-                .appendTo(contenedor)
-                .click(function(){
-                    $('#'+kGrid.div.id).kGrid('pagina','anterior');
-                });
-                
-          var centro = $('<div>').addClass('btn btn-default kpagina')
-                .css('height',$('#'+pk+'pagina_anterior').outerHeight())
-                .appendTo(contenedor);
-                
-          $('<label>').html('Página ').appendTo(centro);
-          $('<input>').attr('id',pk+'pagina')
-            .attr('type','text')
-            .addClass('pagina')
-            .css('width',$('#'+pk+'pagina_anterior').outerWidth())
-            .appendTo(centro)
-            .keyup(function(e){
-                if(e.keyCode === 13){
-                    e.preventDefault();
-                    var pagina = parseInt($('#'+pk+'pagina').val());
-                    if(isNaN(pagina) || pagina<0 || pagina > kGrid.totalPaginas){
-                        $('#'+pk+'pagina').val($('#'+pk+'pagina').data('pagina'));
-                        return;
-                    }
-                    if(pagina!==$('#'+pk+'pagina').data('pagina')){
-                        $('#'+kGrid.div.id).kGrid('pagina',pagina);
-                    }
-                }
-            });
-          var totalPaginas = $('<label>').html(' de ').appendTo(centro);
-          $('<span>').attr('id',pk+'totalPaginas')
-            .appendTo(totalPaginas);
-                
-          $('<button>').attr('id',pk+'siguiente_pagina')
-                .attr('type','button')
-                .addClass('btn btn-default')
-                .html( $('<i>').addClass('fa fa-forward'))
-                .appendTo(contenedor)
-                .click(function(){
-                    $('#'+kGrid.div.id).kGrid('pagina','siguiente');
-                });
-                
-           $('<button>').attr('id',pk+'ultima_pagina')
-                .attr('type','button')
-                .addClass('btn btn-default')
-                .html($('<i>').addClass('fa fa-step-forward'))
-                .appendTo(contenedor)
-                .click(function(){
-                    $('#'+kGrid.div.id).kGrid('pagina','ultima');
-                });
-        },
-
         cambiar_seleccion: function(codigo,estado){
             var kGrid = this;
             kGrid.seleccionados[codigo] = estado;
